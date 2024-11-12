7360a054ef3dca65546c4fb3302d432f9c9847b6
김주호 <billbill415415415@gmail.com>
2024-11-12 14:42:03 +0900

    수정1

---
+++ data/tweet.js
@@ -0,0 +1,68 @@
+let tweets = [
+    {
+        id:'1',
+        name:'김사과',
+        username:'apple',
+        text:'안녕하세요',
+        createdAt: Date.now().toString(),
+        url:'https://img.freepik.com/premium-photo/young-asian-man-her-clean-face-with-fresh-healthy-skin-ai-generated_145713-6656.jpg'
+    },
+    {
+        id:'2',
+        name:'반하나',
+        username:'banana',
+        text:'반갑습니다',
+        createdAt: Date.now().toString(),
+        url:'https://img.freepik.com/premium-photo/young-asian-man-her-clean-face-with-fresh-healthy-skin-ai-generated_145713-6656.jpg'
+    },
+    {
+        id:'3',
+        name:'오렌지',
+        username:'orange',
+        text:'첫 트윗!',
+        createdAt: Date.now().toString(),
+        url:'https://img.freepik.com/premium-photo/young-asian-man-her-clean-face-with-fresh-healthy-skin-ai-generated_145713-6656.jpg'
+    }
+]
+
+// 모든 트윗을 리턴
+export async function getAll(){
+    return tweets
+}
+
+// 아이디에 대한 트윗을 리턴
+export async function getAllByUsername(username){
+    return tweets.filter((tweet) => tweet.username == username)
+}
+
+// 글 번호에 대한 트윗을 리턴
+export async function getById(id){
+    return tweets.find((tweet) => tweet.id === id)
+}
+
+// 트윗을 작성
+export async function create(username, name, text){
+    const tweet = {
+        id: '4',
+        username: username,
+        name: name,
+        text: text,
+        createdAt: Date.now().toString()
+    }
+    tweets = [tweet, ...tweets]
+    return tweet
+}
+
+// 트윗을 변경
+export async function update(id, text){
+    const tweet = tweets.find((tweet) => tweet.id === id)
+    if(tweet){
+        tweet.text = text
+    }
+    return tweet
+}
+
+// 트윗을 삭제
+export async function remove(id){
+    tweets = tweets.filter((tweet) => tweet.id !== id)
+}
